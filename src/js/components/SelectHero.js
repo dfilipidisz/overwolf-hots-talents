b0c536afa44fdda11f6003b6087631a75d90f245
David Filipidisz <dfilipidisz@gmail.com>
2017-02-07 08:59:53 +0100

    Remove unused file and update to es6 classes in some places

--- src/js/components/SelectHero.js
+++ src/js/components/SelectHero.js
@@ -3,66 +3,8 @@ import Select from 'react-select';
 import { connect } from 'react-redux';
 import { HEROES } from '../constants';
 import { talentsChooseHero } from '../actions/talents';
-
-const HeroOption = React.createClass({
-  propTypes: {
-  	children: React.PropTypes.node,
-  	className: React.PropTypes.string,
-  	isDisabled: React.PropTypes.bool,
-  	isFocused: React.PropTypes.bool,
-  	isSelected: React.PropTypes.bool,
-  	onFocus: React.PropTypes.func,
-  	onSelect: React.PropTypes.func,
-  	option: React.PropTypes.object.isRequired,
-  },
-  handleMouseDown(event) {
-  	event.preventDefault();
-  	event.stopPropagation();
-  	this.props.onSelect(this.props.option, event);
-  },
-  handleMouseEnter(event) {
-  	this.props.onFocus(this.props.option, event);
-  },
-  handleMouseMove(event) {
-  	if (this.props.isFocused) return;
-  	this.props.onFocus(this.props.option, event);
-  },
-  render() {
-  	return (
-    <div
-      className={ this.props.className }
-      onMouseDown={ this.handleMouseDown }
-      onMouseEnter={ this.handleMouseEnter }
-      onMouseMove={ this.handleMouseMove }
-      onMouseLeave={ this.handleMouseLeave }
-      title={ this.props.option.title }
-    >
-      <div className={ `hero-portrait ${this.props.option.value}` } />
-      {this.props.children}
-    </div>
-  	);
-  },
-});
-
-const HeroValue = React.createClass({
-
-  propTypes: {
-    children: React.PropTypes.node,
-    placeholder: React.PropTypes.string,
-    value: React.PropTypes.object,
-  },
-
-  render() {
-  	return (
-    <div className='Select-value' title={ this.props.value.title }>
-      <span className='Select-value-label'>
-        <div className={ `hero-portrait ${this.props.value.value}` } />
-        {this.props.children}
-      </span>
-    </div>
-  	);
-  },
-});
+import SelectHeroValue from './SelectHeroValue';
+import SelectHeroOption from './SelectHeroOption';
 
 class SelectHero extends React.Component {
 
@@ -85,15 +27,21 @@ class SelectHero extends React.Component {
     return (
       <Select
         onChange={ this.changedHero }
-        optionComponent={ HeroOption }
+        optionComponent={ SelectHeroOption }
         options={ HEROES }
         placeholder={ <span>Select a Hero</span> }
         value={ this.props.selectedHero }
-        valueComponent={ HeroValue }
+        valueComponent={ SelectHeroValue }
       />
     );
   }
 }
+
+SelectHero.propTypes = {
+  talentsChooseHero: React.PropTypes.func,
+  updateSize: React.PropTypes.func,
+  selectedHero: React.PropTypes.string,
+};
 
 module.exports = connect(
   state => ({ heroes: state.talents.heroes, selectedHero: state.talents.selectedHero }),
